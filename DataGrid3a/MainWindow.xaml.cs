ece9b90b44cb0ffd1bd992dc88d2a0448029b71b
Wirginia Tomczyk <virginiapyt@poczta.onet.pl>
2021-12-21 10:33:50 +0100

    Dodaj pliki projektów.

---
+++ DataGrid3a/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGrid3a
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Produkt> ListaProduktow { get; set; }
+        public ObservableCollection<string> magazyny { get; set; }
+        public string sciezka = "pack://application:,,,/obrazki/";
+        public MainWindow()
+        {
+            InitializeComponent();
+            magazyny = new ObservableCollection<string>()
+            { "Katowice_m1", "Gliwice_m2", "Zabrze_m3"};
+            kombo.ItemsSource = magazyny;
+
+            ListaProduktow = new ObservableCollection<Produkt>();
+            ListaProduktow.Add(new Produkt("aaa", "komputer", "Katowice_m1", 20,new Uri(sciezka+"borowik.jpg")));
+            ListaProduktow.Add(new Produkt("bbb", "monitor", "Gliwice_m2", 30, new Uri(sciezka + "borowik.jpg")));
+            ListaProduktow.Add(new Produkt("ccc", "klawiatura", "Katowice_m1", 25, new Uri(sciezka + "borowik.jpg")));
+            ListaProduktow.Add(new Produkt("ddd", "myszka", "Gliwice_m2", 245, new Uri(sciezka + "kania.jpg")));
+            ListaProduktow.Add(new Produkt("ddd", "myszka", "Gliwice_m2", 245, new Uri(sciezka + "borowik.jpg")));
+            ListaProduktow.Add(new Produkt("ddd", "myszka", "Gliwice_m2", 245, new Uri(sciezka + "szatan.jpg")));
+            ListaProduktow.Add(new Produkt("ddd", "myszka", "Gliwice_m2", 245, new Uri(sciezka + "borowik.jpg")));
+            ListaProduktow.Add(new Produkt("ddd", "myszka", "Gliwice_m2", 245, new Uri(sciezka + "kurka.jpg")));
+            DataContext = this;
+        }
+    }
+}
---
+++ DataGrid3a/Produkt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid3a
+{
+    public class Produkt
+    {
+        public string Symbol { get; set; }
+        public string Nazwa { get; set; }
+        public string Magazyn { get; set; }
+        public int LiczbaSztuk { get; set; }
+
+        public bool Dostepne { get; set; }
+        public Uri Zdjecie { get; set; }
+
+        public Produkt(string symbol, string nazwa, string magazyn, 
+            int liczbaSztuk, Uri zdjecie)
+        {
+            Symbol = symbol;
+            Nazwa = nazwa;
+            Magazyn = magazyn;
+            LiczbaSztuk = liczbaSztuk;
+            Dostepne = true;
+            Zdjecie = zdjecie;
+        }
+    }
+}
